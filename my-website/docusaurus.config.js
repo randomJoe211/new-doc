47fb1bc05db66cad5f7d93a01d15dab36f2df856
randomJoe211 <69501902+randomJoe211@users.noreply.github.com>
2022-10-17 14:26:13 +0800

    Revert "Rename my-website to website"
    This reverts commit 84302a4482e325d2afc3d7c4ee7b2485685441b8.

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/randomJoe211/randomjoe211.github.io/website/',
+            'https://github.com/randomJoe211/randomjoe211.github.io/my-website/',
         },
         blog: false,
         pages: false,
