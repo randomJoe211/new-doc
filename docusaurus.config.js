9e7d8d8e4e1cc40f226634861bc9f6e6354b06f4
randomJoe211 <69501902+randomJoe211@users.noreply.github.com>
2022-10-17 15:59:10 +0800

    Search test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,19 +64,22 @@ const config = {
     ],
   ],
   themes: [
-    // This is the search plugin presented as a theme.
-    // https://github.com/easyops-cn/docusaurus-search-local
+    // ... Your other themes.
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
-      {
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
         // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
+        docsRouteBasePath: '/',
+        docsDir: '/',
+        highlightSearchTermsOnTargetPage: true,
         // For Docs using Chinese, The `language` is recommended to set to:
         // ```
         // language: ["en", "zh"],
         // ```
-      },
+      }),
     ],
   ],
 
