253b89fbc768ed9f75081c4cdb09a85517b613eb
randomJoe211 <69501902+randomJoe211@users.noreply.github.com>
2022-11-04 15:08:01 +0800

    Update PDFMerger.js

--- src/components/PDFMerger.js
+++ src/components/PDFMerger.js
@@ -3,10 +3,10 @@ const PDFMerger = require('pdf-merger-js');
 var merger = new PDFMerger();
 
 (async () => {
-  await merger.add('./pdf/NebulaGraphDatabase.pdf');  //merge all pages. parameter is the path to file and filename.
-  await merger.add('./pdf/NebulaGraphCloud.pdf'); 
+  await merger.add('./pdf/NebulaGraphDatabase_cn.pdf');  //merge all pages. parameter is the path to file and filename.
+  await merger.add('./pdf/NebulaGraphCloud_cn.pdf'); 
 
-  await merger.save('./pdf/NebulaGraphDocsInAll.pdf'); //save under given name and reset the internal document
+  await merger.save('./pdf/NebulaGraphDocsInAll_cn.pdf'); //save under given name and reset the internal document
   
   // Export the merged PDF as a nodejs Buffer
   // const mergedPdfBuffer = await merger.saveAsBuffer();
