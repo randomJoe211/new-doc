e6eee51a33201cf0dae6393bf22be04159ec2bef
randomJoe211 <69501902+randomJoe211@users.noreply.github.com>
2022-11-11 13:33:47 +0800

    Update macros

--- src/components/Macros.js
+++ src/components/Macros.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { cloud, studio } from '@site/src/config.js';
+// Configure product objects in src/macrosConfig.js first, then
+// follow the examples in this file to add macro tags.
+// To use the macro tags in a Markdown file, import the JS modules in the MD file as follows:
+// import { MacroCloudRelease, StudioMacros } from '@site/src/components/Macros'
+// import { nebula, cloud, studio } from '@site/src/macrosConfig';
+// Then use the macro tags in the content like:
+// NebulaGraph <MacroNebulaRelease></MacroNebulaRelease>
+// which will be rendered as NebulaGraph 3.3.0 (if the macro value is 3.3.0).
 
-export function CloudMacros() {
+import React from 'react';
+import { nebula, cloud, studio, dashboard, explorer } from '@site/src/macrosConfig';
+
+export function MacroCloudRelease() {
     return (
       <span>{cloud.release}</span>
     );
   }
 
-export function StudioMacros() {
+export function MacroStudioRelease() {
   return (
     <span>{studio.release}</span>
   );
 }
+
+export function MacroNebulaRelease() {
+  return (
+    <span>{nebula.release}</span>
+  );
+}
+
+// To use macros in a code block, you must use the HTML tags pre and code as follows:
+// <pre>
+// <code>
+// wget https://oss-cdn.nebula-graph.com.cn/package/<MacroNebulaRelease></MacroNebulaRelease>/nebula-graph-<MacroNebulaRelease></MacroNebulaRelease>.el7.x86_64.rpm
+// </code>
+// </pre>
--- src/macrosConfig.js
+++ src/macrosConfig.js
@@ -1,24 +1,27 @@
+// To add macros, add product objects and the needed key-value pairs.
+// Then modify src/components/Macros.js.
+
 export const cloud = {
     release: 'v3.0.0',
-    name: 'Cloud'
+    branch: 'release-3.3'
 }
 
 export const studio = {
     release: 'v3.1.0',
-    name: 'Studio'
+    branch: 'release-3.3'
 }
 
 export const dashboard = {
     release: 'v3.0.0',
-    name: 'Cloud'
+    branch: 'release-3.3',
 }
 
 export const explorer = {
     release: 'v3.0.0',
-    name: 'Cloud'
+    branch: 'release-3.3'
 }
 
 export const nebula = {
     release: 'v3.0.0',
-    name: 'Cloud'
+    branch: 'release-3.3'
 }
